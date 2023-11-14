ecc097103b15642e0e01fb85c8627a0d4043c0ac
simonhagos <simonhagos@cmail.carleton.ca>
2023-11-14 08:07:23 -0500

    1, Extracted getbaseamount to be variable.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         lifeSite lifesite = new lifeSite(1.2, 3.0,0.7);
-        System.out.println("lifesite's baseamount is: " + lifesite.getbaseamount());
+        double getbaseamount = lifesite.getbaseamount();
+        System.out.println("lifesite's baseamount is: " + getbaseamount);
     }
 }
