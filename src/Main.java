eeeff8092afa44a2d8a08b69f91f64494773e192
simonhagos <simonhagos@cmail.carleton.ca>
2023-11-14 08:09:31 -0500

    2, introduced variable s,

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         lifeSite lifesite = new lifeSite(1.2, 3.0,0.7);
         double getbaseamount = lifesite.getbaseamount();
-        System.out.println("lifesite's baseamount is: " + getbaseamount);
+        String s = "lifesite's baseamount";
+        System.out.println(s + " is: " + getbaseamount);
     }
 }
