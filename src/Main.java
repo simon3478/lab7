5c15bb12a3da658237a9d1ad8af99a291d49b71a
simonhagos <simonhagos@cmail.carleton.ca>
2023-11-14 08:50:20 -0500

    Safe deleting extract method

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,7 @@ public class Main {
         lifeSite lifesite = new lifeSite(unit, 3.0,0.7);
         double getbaseamount = lifesite.getbaseamount();
         System.out.println(s + " is: " + getbaseamount);
-        extracted(s);
     }
 
-    private static void extracted(String s) {
-        double unit1 = 3.4;
-        ResidentialSite Resident = new ResidentialSite(unit1, 4.2, 7.8);
-        System.out.println(s + Resident.getbaseamount());
-    }
+
 }
