dbac1a32f348de444612e434c30d78a08045cf49
simonhagos <simonhagos@cmail.carleton.ca>
2023-11-14 08:18:01 -0500

    3, substring of argument lifeStie is extracted

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        lifeSite lifesite = new lifeSite(1.2, 3.0,0.7);
+        double unit = 1.2;
+        lifeSite lifesite = new lifeSite(unit, 3.0,0.7);
         double getbaseamount = lifesite.getbaseamount();
         String s = "lifesite's baseamount";
         System.out.println(s + " is: " + getbaseamount);
+
     }
 }
