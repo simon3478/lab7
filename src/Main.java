ade72a8c6cc87689aacd9f064ed3303ca4b68283
simonhagos <simonhagos@cmail.carleton.ca>
2023-11-14 07:31:06 -0500

    created main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LifeSite lifesite = new LifeSite(1.2, 3.0,0.7);
+        System.out.println("lifesite's baseamount is: " + lifesite.getBaseAmount());
     }
 }
